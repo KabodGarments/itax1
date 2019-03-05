a7cc58b387227b2be726ddc06e0cd77bb9694948
KabodGarments <kabodgarments@gmail.com>
2019-03-05 15:37:25 +0300

    js edits to make the numbers right

--- js/app.js
+++ js/app.js
@@ -1,34 +1,48 @@
+//Business Logic//
+//select all inputs and store them into  variables//
+//collect all input from html UI//
 let inputs = document.querySelectorAll("input");
+
+//return output via UI//
 let outputs = document.querySelectorAll("#output>div>span");
 
+//create a functio//
+//create an array of 5 tax ranges//
+//anything over 466,704 k we use number.MAX_VALUE//
+//create an array of 5 minimum taxes for each tax range/bracket//
+//create an array of 5 tax rates//
+//create an empty arrayan call it "input values"//
 function cal_Tax() {
   let Input_Values = [];
   let Income_Ranges = [0, 121968, 236880, 351792, 466704, Number.MAX_VALUE];
-  let Minimum_Tax = [0, 12196.8, 28425.6, 70358.4, 116767,140011.2];
-  let Tax_Rates = [0.1, 0.15, 0.20, 0.25, 0.30, 0.30];
+  let Minimum_Tax = [12196.8, 28425.6, 70358.4, 116767,140011.2];
+  let Tax_Rates = [0.1, 0.15, 0.20, 0.25, 0.30,];
   let Tax_Return;
   let Tax_Payable;
+//create for loop//
+//collect all the input values above and store to empty array "input values"/
 
   for (let i = 0; i < inputs.length; i++) {
-    let value = inputs[i].value;
-    if (value === "") {
+    let value = inputs[i].value; //create variable and loop goes through each input and stores the value in variable "value"//
+    if (value === "") { //if value has empty string push an integer 0, else the value entered. use push to get integers and not strings//
       Input_Values.push(Number(0));
     } else {
       Input_Values.push(Number(value));
     }
   }
   let Taxable_Income = Input_Values[0] + Input_Values[1] - Input_Values[3] - Input_Values[4] - Input_Values[5] - Input_Values[6] - Input_Values[7];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {//create another for loop and run loop 5 via condition "5" times as we have five classes/columns for our data which equal to 5 different conditions//
     if (
       Taxable_Income > Income_Ranges[i] &&
       Taxable_Income <= Income_Ranges[i + 1]
     ) {
       let Gross_Tax =
         (Taxable_Income - Income_Ranges[i]) * Tax_Rates[i] + Minimum_Tax[i];
-      Tax_Payable = (Gross_Tax - Input_Values[4]).toFixed(0);
+      Tax_Payable = (Gross_Tax - Input_Values[8]).toFixed(0);
       Tax_Return = (Tax_Payable - 0.12 * Input_Values[2]).toFixed(0);
     }
   }
+
 
   let foo = Number(Input_Values[0] + Input_Values[1]);
   let total = document.getElementById("total");
